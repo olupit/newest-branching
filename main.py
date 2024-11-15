bba320418d9e70d8094a338138b2623a39ba8489
Your Name <you@example.com>
2024-11-15 14:37:03 +0000

    New functione

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def greet_user(name):
     """Greets the user by name."""
     print(f"Hello, {name}!Welcome!")
+    
+def calculate_sum(a, b): 
+    return a + b
+
 
 if __name__ == "__main__":
     user_name = input("Enter your name: ")
